e3392c04bddf6f3a4e33be0fe5e6db28564ffd55
Rafiqul Islam <rafiquljakir@gmail.com>
2024-12-17 22:42:13 +0600

    project skeleton upload

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin-dashboard', [AdminController::class, 'index'])->name('admin.index');
+});
