75dbddd59fbc17fa0700fb651ac76ae283138c96
Rafiqul Islam <rafiquljakir@gmail.com>
2024-12-21 17:00:07 +0600

    add new brand in admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use PharIo\Manifest\Extension;
+use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -15,5 +19,39 @@ class AdminController extends Controller
     {
         $brands = Brand::orderBy('id', 'DESC')->paginate(10);
         return view('admin.brands', compact('brands'));
+    }
+    public function add_brand()
+    {
+        return view('admin.brand_add');
+    }
+
+    public function brand_store(Request $request)
+    {
+        $request->validate([
+            "name" => 'required',
+            "slug" => 'required|unique:brands,slug',
+            "image" => 'mimes:png,jpg,jpeg|max:2048'
+
+        ]);
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->slug);
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+        $this->GenerateBrandThumbnailsImage($image, $file_name);
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully!');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/brands');
+        $img = Image::read($image->path());
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
     }
 }
